f993cd37ca7b1af487d923ef61b73e01dda4b032
GreenpeaceKT <jymi4406@yahoo.co.jp>
2021-08-07 19:58:46 +0900

    テストしてない

--- src/greenpeacekt/KillAndGetMoneyForMoneysystem/Main.php
+++ src/greenpeacekt/KillAndGetMoneyForMoneysystem/Main.php
@@ -2,10 +2,41 @@
 
 declare(strict_types=1);
 
-namespace greenpeacekt\KillAndGetMoneyForMoneysystem;
+namespace greenpeacekt\KillAndGiveMoneyForMoneysystem;
 
+use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 
-class Main extends PluginBase{
+use pocketmine\utils\Config;
+
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\Listener;
+
+use metowa1227\moneysystem\api\core\API;
+
+class Main extends PluginBase implements Listener{
+
+private $set,$amount;
+
+ public function onEnable() {
+         if (!file_exists($this->getDataFolder())) mkdir($this->getDataFolder());
+         $this->getServer()->getPluginManager()->registerEvents($this, $this);
+ 
+         $this->set = new Config($this->getDataFolder() . "KillGiveMoney.yml", Config::YAML, array("amount" => array()));
+         $this->$amount = $this->set->get("amount");
+     }
+
+
+
+ public function onDeath(PlayerDeathEvent $ev){
+    	$entity = $ev->getPlayer();
+    	$cause = $entity->getLastDamageCause();
+    	if($cause instanceof EntityDamageByEntityEvent){
+    	  if($this->moneysystem)
+    		$damager = $cause->getDamager();
+    		$damagername = $damager->getName();
+    		$this->API::getInstance()->increase($player, $amount);
+    	}
+    }
 
 }
